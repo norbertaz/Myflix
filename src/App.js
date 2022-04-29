00c367aea53748708f982580c21ed6aee6b77c48
Norbert <kamreal108@gmail.com>
2022-04-30 01:55:15 +0200

    movies display, just searching bar to do

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+
+import Movie from './components/Movie'
+
+
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=ac9f196863b883b15565a053074f4a49&query&page=1"
+// const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=ac9f196863b883b15565a053074f4a49&query="
 
 function App() {
+  const [movies, setMovies] = useState([])
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    fetch(FEATURED_API)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data.results)
+        setMovies(data.results)
+      })
+
+    
+  },[])
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+  }
+  const handleChange = (e) => {
+    setSearch(e.target.value)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="bg-indigo-600 p-4 flex justify-end">
+        <form onSubmit={handleSubmit}></form>
+        <input 
+        value={search}
+        onChange={handleChange}
+        type="text" 
+        placeholder="Search" 
+        className="py-3 px-6 text-lg text-white rounded-full bg-transparent border-solid border-2 border-indigo-900 font-sans"/>
       </header>
+    <div className="flex flex-wrap justify-around mt-6">
+        {movies.map(movie => <Movie key={movie.id} {...movie}/>)}
     </div>
+    </>
   );
 }
 
---
+++ src/components/Movie.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const IMAGE_API = "https://image.tmdb.org/t/p/w1280"
+const Movie = ({title, poster_path, overview, vote_average}) => (
+    <div className="movie w-72 m-4 rounded-sm shadow-lg shadow-black/40 overflow-hidden relative group">
+        <div className="movie-header text-white ">
+            <img className="max-w-full h-[432px]" src={IMAGE_API + poster_path} alt={title} />
+            <div className="movie-info h-24 flex items-center justify-between p-6 bg-indigo-600 ">
+                <h3 className="m-0 font-semibold">{title}</h3>
+                <span className="text-center p-1 font-bold w-8 bg-indigo-900 rounded">{vote_average}</span>
+            </div>
+            <div className="rounded-t-sm shadow-lg shadow-black/40 movie-overview absolute bottom-0 left-0 right-0 bg-white text-indigo-600 p-6 translate-y-full group-hover:translate-y-0 transition duration-300">
+                <h2 className="font-semibold ">Overview:</h2>
+                <p>{overview}</p>
+            </div>
+
+        </div>
+    </div>
+)
+
+export default Movie
